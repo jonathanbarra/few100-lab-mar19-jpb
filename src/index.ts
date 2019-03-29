0b5b24f77c3372aa907dba65b2f3ceea803a4a59
jonathanbarra <jonathanbarra@gmail.com>
2019-03-29 20:34:59 +0000

    March 29 Display Input Finally Working

--- src/index.ts
+++ src/index.ts
@@ -5,33 +5,28 @@ console.log("we will PARTY HARD!");
 
 let billAmountInput:HTMLInputElement= document.querySelector('#bill-amount-input');
 //let billAmount:number = document.getElementById('bill-amount').nodeType.valueOf();
-let billAmount:number = parseInt(billAmountInput.value);
+//let billAmount:number = parseInt(billAmountInput.value);
 let displayedBillAmount = document.querySelector('#displayed-bill-amount');
 let  tipPercentageMultiplier = 0;
 let tipPercentageButtonId = "";
 const horizontalTipPercentageButtons = document.querySelector("#tip-amount-buttons" );
 const tipPercentageButtons = horizontalTipPercentageButtons.querySelectorAll("li");
-/* 
-console.log(tipPercentageButtons[0].id);
-console.log(tipPercentageButtons[1].id);
-console.log(tipPercentageButtons[2].id); */
 
-billAmountInput.addEventListener('click', displayBillAmountInput);
+billAmountInput.addEventListener('blur', displayBillAmountInput); 
 
-tipPercentageButtons.forEach((tipAmountButton) =>{
+ tipPercentageButtons.forEach((tipAmountButton) =>{
     tipAmountButton.addEventListener('click', setTipPercentageWithClick)
     console.log("this is adding an event listener to the tip percentage buttons?")
 })
 
-console.log(`the value of  billAmountInput is<b>${billAmountInput}</b>`);
-console.log(`the value of  billAmount<b>${billAmount}</b>`);
-
 function displayBillAmountInput() {
+    let billAmount:number = parseInt(billAmountInput.value);
+    console.log("The displayBillAmountInput method was called");
     if(billAmount  >= 0){
         displayedBillAmount.innerHTML= billAmount.toString();
         console.log(`display bill amount was called with value of <b>${billAmount}</b>`);
         }
-}
+} 
 function setTipPercentageWithClick(){
     const button = this as HTMLLIElement;
     console.log(button.id);
@@ -59,8 +54,7 @@ function setTipPercentageWithClick(){
         default:
             break;
     }
-    if(billAmount  >= 0){
-
-    }
+    //if(billAmount  >= 0){
+    //}
 });
 }
